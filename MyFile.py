950a8b19aca1ea32b19d49d94ce675012ed5570d
Pratik <pratikchilate073@gmail.com>
2022-05-17 23:57:12 +0530

    Commented fibo funtion and added swap_case funtion

--- MyFile.py
+++ MyFile.py
@@ -1,12 +1,22 @@
-def fibo(term):
-    a,b = 0,1
+# def fibo(term):
+#     a,b = 0,1
 
-    while a < term:
-        print(a)
+#     while a < term:
+#         print(a)
 
-        a,b = b,a+b
+#         a,b = b,a+b
 
 
-n = int(input("Enter the term: "))
-fibo(n)
+# n = int(input("Enter the term: "))
+# fibo(n)
 
+def swap(c):
+    pass
+
+
+
+def swap_case(s):
+    return "".join(map(swap , s))
+
+
+swap_case("Hello World")
