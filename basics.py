3ca8fc05b871bb6b4466a490115b62cc81cbf619
Pratik <pratikchilate073@gmail.com>
2022-05-18 14:15:29 +0530

    commented maximum_of_Two function and added Maximum_of_Three function

--- basics.py
+++ basics.py
@@ -17,11 +17,23 @@
 
 # function to find Maximum amog two numbers
 
-def Maximum_of_Two(a,b):
-    result = a if a > b else b
+# def Maximum_of_Two(a,b):
+#     result = a if a > b else b
+#     return result
+
+
+# n1,n2 = map(eval,(input("Enter two numbers to find maximum: ").split()))
+
+# print(Maximum_of_Two(n1,n2))
+
+
+# function to find Maximum among three
+
+def Maximum_of_Three(a,b,c):
+    result = a if a > b  and a > c else b if b > c  else c
     return result
 
 
-n1,n2 = map(eval,(input("Enter two numbers to find maximum: ").split()))
+n1,n2,n3 = map(eval,(input("Enter three numbers to find maximum: ").split()))
 
-print(Maximum_of_Two(n1,n2))
+print(Maximum_of_Three(n1,n2,n3))
