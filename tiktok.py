5780e7b99bcd32ec07f5996b25454f46c7130f84
Pratik <pratikchilate073@gmail.com>
2022-05-19 09:18:08 +0530

    added tiktok.py

---
+++ tiktok.py
@@ -0,0 +1,15 @@
+def func(value):
+  if value % 3 == 0 and value % 7 == 0:
+      return "tiktok"
+  if value % 3 == 0:
+      return "tik"
+  if value % 7 == 0:
+      return "tok"
+  return value
+
+
+
+n = int(input("Enter you number: "))
+print(func(n))
+
+
